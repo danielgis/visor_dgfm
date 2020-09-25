2ba3bca246e03709dcf66b563bd6007294cd59c1
danielgis <daniel030891@gmail.com>
2020-09-25 17:24:09 -0500

    Se removio la tabla de atributos

--- widgets/Reporte_wgt/nls/strings.js
+++
@@ -1,9 +0,0 @@
-define({
-  root: {
-    _widgetLabel: 'Reporte',
-    widgetTitle: 'Reporte',
-    description: 'Widget que permite la impresion de un mapa con la visualizacion actual'
-  }
-  // add supported locales below:
-  // , "zh-cn": true
-});
--- widgets/presentacion_wgt/nls/strings.js
+++
@@ -1,9 +0,0 @@
-define({
-  root: {
-    _widgetLabel: 'presentacion_wgt',
-    widgetTitle: 'presentacion_wgt',
-    description: 'A custom Web AppBuilder widget.'
-  }
-  // add supported locales below:
-  // , "zh-cn": true
-});
