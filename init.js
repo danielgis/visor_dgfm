9dc0cd240ad7ef7ace5c45d0f322579df4d9973d
danielgis <daniel030891@gmail.com>
2020-09-28 01:57:16 -0500

    Correccion de errores en intenet explorer

--- init.js
+++ init.js
@@ -98,6 +98,7 @@ var ie = (function() {
             'https://cdn.jsdelivr.net/npm/bulma@0.9.0/css/bulma.min.css',
             'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
             'https://use.fontawesome.com/releases/v5.3.1/js/all.js',
+            "https://cdnjs.cloudflare.com/ajax/libs/es6-shim/0.34.1/es6-shim.js",
             // 'https://cdn.jsdelivr.net/npm/chart.js@2.7.3/dist/Chart.min.js',
             // 'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js',
             // window.path + 'libs/chartjs/Chart.min.css',
